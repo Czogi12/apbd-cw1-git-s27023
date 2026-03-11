f31ecf50c7799e9a13a067c032e131e95f47d74e
Maciej Nowakowski <s27023@pjwstk.edu.pl>
2026-03-11 09:24:05 +0100

    implemented CalculateAverage in Statistics helper

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -1,1 +1,9 @@
-public class StatisticsHelper {}
+public class StatisticsHelper {
+    public double CalculateAverage(int[] values) {
+        double sum = 0;
+        foreach(int value in values) {
+            sum += value;
+        }
+        return sum / values.Length;
+    }
+}
