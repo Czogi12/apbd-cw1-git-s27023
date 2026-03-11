04a61e0074c117c28876f2d6c6ec2827ad69c5c5
Maciej Nowakowski <s27023@pjwstk.edu.pl>
2026-03-11 09:25:32 +0100

    tested statistics calculations

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
 ﻿Console.WriteLine(StringHelper.toUpperCase("Hello, World!"));
+
+
+StatisticsHelper statisticsHelper = new StatisticsHelper();
+Console.WriteLine(statisticsHelper.CalculateAverage(new int[]{1,2,3}));
